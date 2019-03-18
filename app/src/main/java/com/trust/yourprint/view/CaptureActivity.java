4c71bf1555be101dbada3bf36176b42d22157fad
Nelson Briceño <nbriceno@jumpitt.com>
2019-03-18 18:45:33 -0300

    Integracion de opencv

--- app/src/main/java/com/trust/yourprint/view/CaptureActivity.java
+++ app/src/main/java/com/trust/yourprint/view/CaptureActivity.java
@@ -2,8 +2,11 @@ package com.trust.yourprint.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.trust.yourprint.R;
+
+import org.opencv.android.OpenCVLoader;
 
 public class CaptureActivity extends AppCompatActivity {
 
@@ -11,5 +14,12 @@ public class CaptureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture);
+
+        if (OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "OpenCV cargo ctmre!!!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "OpenCV no cargo ctmre!!!", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
