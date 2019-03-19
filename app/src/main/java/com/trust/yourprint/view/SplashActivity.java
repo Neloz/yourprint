7153ce76f8ec20481acf79361e71b612cff074ca
Nelson Briceño <nbriceno@jumpitt.com>
2019-03-19 11:23:28 -0300

    Permisos integrados

--- app/src/main/java/com/trust/yourprint/view/SplashActivity.java
+++ app/src/main/java/com/trust/yourprint/view/SplashActivity.java
@@ -1,12 +1,25 @@
 package com.trust.yourprint.view;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.trust.yourprint.MainActivity;
 import com.trust.yourprint.R;
+
+import static android.Manifest.permission.CAMERA;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -15,14 +28,61 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         //Declaramos un Handler que hace de unión entre el hilo principal y el secundario
-        Handler handler = new Handler();
 
-        //Llamamos al método postDelayed
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if ((checkSelfPermission(CAMERA) != PackageManager.PERMISSION_GRANTED)) {
+
+                checkCameraPermission();
+            }else{
+                gotoMain(2000);
+            }
+
+        }else{
+            gotoMain(2000);
+        }
+
+
+    }
+
+    private void checkCameraPermission() {
+
+
+        Dexter.withActivity(this)
+                .withPermission(CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        //Llamamos al método postDelayed
+                        gotoMain(2000);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(SplashActivity.this, "Entrega los permisos cochino ctmre!!!", Toast.LENGTH_LONG).show();
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {/* ... */}
+                }).check();
+    }
+
+    private void gotoMain(int delay) {
+        final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
             }
-        }, 2000); // 2 segundos de "delay"
+        }, delay); // 2 segundos de "delay"
+    }
+
+    private void openSetting(){
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
     }
 }
